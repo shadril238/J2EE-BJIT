8ac880d213de11987e5e0142367e7c45a1a81b13
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-08-21 14:05:53 +0600

    JSP-Servlet Demo

---
+++ JavaEE/Codes_Section/JSP_Servlet-Demo/src/main/java/DB_Utilities/ConnectJDBC.java
@@ -0,0 +1,32 @@
+package DB_Utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectJDBC {
+    String url = "jdbc:mysql://localhost/expense_manager";
+    String user = "root";
+    String pass = "";
+    Connection connection;
+    public ConnectJDBC()  {
+        this.connect();
+    }
+    public void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, pass);
+            System.out.println("successfully created Connection object and connected jdbc");
+        } catch (SQLException e) {
+            System.out.println("error");
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+
+//    Statement statement = connection.createStatement();
+}
