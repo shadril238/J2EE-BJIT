efcf7844ec005ee5bdce29549c852746ee97790d
shadril238 <shadrilhassan@outlook.com>
2023-08-23 01:17:09 +0600

    first spring boot application

---
+++ JavaEE/Codes_Section/FirstSpringBootApplication/src/main/java/com/shadril238/FirstSpringBootApplication/rest/FunRestController.java
@@ -0,0 +1,13 @@
+package com.shadril238.FirstSpringBootApplication.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello shadril238!";
+    }
+}
---
+++ JavaEE/Codes_Section/FirstSpringBootApplication/src/test/java/com/shadril238/FirstSpringBootApplication/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadril238.FirstSpringBootApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
