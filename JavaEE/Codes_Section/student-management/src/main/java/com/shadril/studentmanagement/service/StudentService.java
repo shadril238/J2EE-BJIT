82475dd9417435ed2d8353d68d85b4fa7b7f85e3
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-08-29 16:56:58 +0600

    day-18

--- JavaEE/Codes_Section/student-management/src/main/java/com/shadril/studentmanagement/service/StudentService.java
+++ JavaEE/Codes_Section/student-management/src/main/java/com/shadril/studentmanagement/service/StudentService.java
@@ -1,4 +1,17 @@
 package com.shadril.studentmanagement.service;
 
+import com.shadril.studentmanagement.model.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class StudentService {
+    List<Student> studentList = new ArrayList<>();
+    @PostConstruct
+    public void init(){
+        studentList.add(new Student(101, "Shadril", "shadril@gmail.com", "10-09-2000", "Male", "01754402481", "Bashundhara R/A, Dhaka", "Information Systems", "20-01-2020", "04-09-2023", 3.94));
+    }
 }
