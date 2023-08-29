a29a3ba6729dbf48d018d94f12f8f98881aee31a
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-08-29 16:28:46 +0600

    model - Student

--- JavaEE/Codes_Section/student-management/src/main/java/com/shadril/studentmanagement/model/Student.java
+++ JavaEE/Codes_Section/student-management/src/main/java/com/shadril/studentmanagement/model/Student.java
@@ -1,4 +1,122 @@
 package com.shadril.studentmanagement.model;
 
+import java.time.LocalDate;
+
 public class Student {
+    private Integer id;
+    private String name;
+    private String email;
+    private String dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String address;
+    private String major;
+    private String enrollmentDate;
+    private String graduationDate;
+    private Double cgpa;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name, String email, String dateOfBirth, String gender, String phoneNumber, String address, String major, String enrollmentDate, String graduationDate, Double cgpa) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.major = major;
+        this.enrollmentDate = enrollmentDate;
+        this.graduationDate = graduationDate;
+        this.cgpa = cgpa;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(String enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public String getGraduationDate() {
+        return graduationDate;
+    }
+
+    public void setGraduationDate(String graduationDate) {
+        this.graduationDate = graduationDate;
+    }
+
+    public Double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(Double cgpa) {
+        this.cgpa = cgpa;
+    }
 }
