00dc6ffa979adb84f334b04a2ea8438f12890db4
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-08-30 12:22:46 +0600

    global-exception-handling (init)

---
+++ JavaEE/Codes_Section/global-exception-handling/src/test/java/com/shadril/globalexceptionhandling/GlobalExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadril.globalexceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
