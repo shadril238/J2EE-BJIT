c42c314d6e0292be87d1bce60059da5d284641a2
shadril238 <shadrilhassan@outlook.com>
2023-09-06 22:06:44 +0600

    java spring security - init

---
+++ JavaEE/Codes_Section/java-spring-security/src/main/java/com/shadril/javaspringsecurity/JavaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.shadril.javaspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ JavaEE/Codes_Section/java-spring-security/src/test/java/com/shadril/javaspringsecurity/JavaSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadril.javaspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
