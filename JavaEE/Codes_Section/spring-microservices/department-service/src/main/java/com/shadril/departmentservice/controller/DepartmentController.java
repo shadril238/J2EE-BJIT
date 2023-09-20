3b26746b7ca6a74356904e3df5a0981ce6cc3c42
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-09-20 13:32:26 +0600

    microservices

--- JavaEE/Codes_Section/spring-microservices/department-service/src/main/java/com/shadril/departmentservice/controller/DepartmentController.java
+++ JavaEE/Codes_Section/spring-microservices/department-service/src/main/java/com/shadril/departmentservice/controller/DepartmentController.java
@@ -1,2 +1,43 @@
-package com.shadril.departmentservice.controller;public class DepartmentController {
+package com.shadril.departmentservice.controller;
+
+import com.shadril.departmentservice.model.Department;
+import com.shadril.departmentservice.repository.DepartmentRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+
+    private static final Logger LOGGER
+            = (Logger) LoggerFactory.getLogger(DepartmentController.class);
+
+    @Autowired
+    private DepartmentRepository repository;
+
+//    @Autowired
+//    private EmployeeClient employeeClient;
+
+    @PostMapping
+    public Department add(@RequestBody Department department) {
+        LOGGER.info("Department add: {department}");
+        return repository.addDepartment(department);
+    }
+
+    @GetMapping
+    public List<Department> findAll() {
+        LOGGER.info("Department find");
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Department findById(@PathVariable Long id) {
+        LOGGER.info("Department find: id={}" + id);
+        return repository.findById(id);
+    }
+
 }
--- JavaEE/Codes_Section/spring-microservices/department-service/src/main/java/com/shadril/departmentservice/model/Employee.java
+++ JavaEE/Codes_Section/spring-microservices/department-service/src/main/java/com/shadril/departmentservice/model/Employee.java
@@ -1,2 +1,5 @@
-package com.shadril.departmentservice.model;public record Employee() {
+package com.shadril.departmentservice.model;
+
+public record Employee(Long id, Long departmentId, String name, Integer age, String position) {
+
 }
