631621f92854c76c9be3c31c6e9bf4090075d44f
Shadril Hassan Shifat <shadrilhassan@outlook.com>
2023-10-15 21:22:33 +0600

    react ref codes

---
+++ FrontEnd/React/class-references-code/frontend-training-java-batch-02/src/utils/constants.js
@@ -0,0 +1,10 @@
+export const PERSON_INFO = {
+  name: "Angkon Kumar Roy",
+  email: "a@gmail.com",
+  phone: "123456",
+  address: {
+    street: "street Details",
+    city: "Dhaka",
+  },
+  tags: ["1", "tag2"],
+};
