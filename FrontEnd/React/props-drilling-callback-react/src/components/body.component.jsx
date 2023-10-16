590cd8ae28262184af1a49879e0c06d704c1825f
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-10-16 16:35:57 +0600

    useState, useEffect, axios, context api, props drilling, callbacks...event propagration nedd to impl

--- FrontEnd/React/props-drilling-callback-react/src/components/body.component.jsx
+++ FrontEnd/React/props-drilling-callback-react/src/components/body.component.jsx
@@ -1,12 +1,10 @@
 import { useContext, useState } from 'react';
 import './body.component.css';
 import { ProductContext } from '../App';
-import ButtonComponent from './button.component.jsx';
+import ButtonComponent from './button.component.jsx'; 
 
 const BodyComponent = () => {
-    // useContext
     const { data, loading } = useContext(ProductContext);
-    // useState
     const [selectedProduct, setSelectedProduct] = useState(null);
 
     const showDetails = (product) => {
@@ -17,44 +15,49 @@ const BodyComponent = () => {
         setSelectedProduct(null);
     };
 
+    const handleProductDetailsClick = (product) => {
+        alert(`Display details for product: ${product.title}`);
+    };
+
     return (
         <div className="body">
-        {loading ? (
-            <p>Loading data...</p>
-        ) : (
-            <ul className="product-list">
-            {data.map((item) => (
-                <li key={item.id} className="product-box">
-                <div className="product-image">
-                    <img
-                    src={item.thumbnail}
-                    alt={item.title}
-                    style={{ width: '200px', height: '200px' }}
-                    />
-                </div>
-                <div className="product-details">
-                    <h2 className="product-title">{item.title}</h2>
-                    <p className="product-price">Price: ${item.price}</p>
-                    <ButtonComponent
-                    isDetailsShown={selectedProduct === item}
-                    onShowDetails={() => showDetails(item)}
-                    onHideDetails={hideDetails}
-                    />
-                    {selectedProduct === item && (
-                    <div className="product-more-details">
-                        <p>Description: {item.description}</p>
-                        <p>Discount: {item.discountPercentage}%</p>
-                        <p>Rating: {item.rating}</p>
-                        <p>Stock: {item.stock}</p>
-                        <p>Brand: {item.brand}</p>
-                        <p>Category: {item.category}</p>
-                    </div>
-                    )}
-                </div>
-                </li>
-            ))}
-            </ul>
-        )}
+            {loading ? (
+                <p>Loading data...</p>
+            ) : (
+                <ul className="product-list">
+                    {data.map((item) => (
+                        <li key={item.id} className="product-box">
+                            <div className="product-image">
+                                <img
+                                    src={item.thumbnail}
+                                    alt={item.title}
+                                    style={{ width: '200px', height: '200px' }}
+                                />
+                            </div>
+                            <div className="product-details">
+                                <h2 className="product-title">{item.title}</h2>
+                                <p className="product-price">Price: ${item.price}</p>
+                                <ButtonComponent
+                                    isDetailsShown={selectedProduct === item}
+                                    onShowDetails={() => showDetails(item)}
+                                    onHideDetails={hideDetails}
+                                    onProductDetailsClick={() => handleProductDetailsClick(item)}
+                                />
+                                {selectedProduct === item && (
+                                    <div className="product-more-details">
+                                        <p>Description: {item.description}</p>
+                                        <p>Discount: {item.discountPercentage}%</p>
+                                        <p>Rating: {item.rating}</p>
+                                        <p>Stock: {item.stock}</p>
+                                        <p>Brand: {item.brand}</p>
+                                        <p>Category: {item.category}</p>
+                                    </div>
+                                )}
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
