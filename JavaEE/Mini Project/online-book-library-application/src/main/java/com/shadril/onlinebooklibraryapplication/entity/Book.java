07773659f2c575883ea1273809afeb84c4c49d65
Shadril Hassan Shifat <shadrilhassan@outlook.com>
2023-10-22 19:15:12 +0600

    modified for frontend

--- JavaEE/Mini Project/online-book-library-application/src/main/java/com/shadril/onlinebooklibraryapplication/entity/Book.java
+++ JavaEE/Mini Project/online-book-library-application/src/main/java/com/shadril/onlinebooklibraryapplication/entity/Book.java
@@ -16,6 +16,10 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private String description;
+    private String imgUrl;
+    private String pageLength;
+    private String language;
     private String author;
     private String status;
     private Boolean isActive;
